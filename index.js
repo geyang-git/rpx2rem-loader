5de4d115e967524a4fb11d6e5c707a7bbb2d7362
GY <284763415@users.noreply.github.com>
2020-07-18 01:59:51 +0800

    test

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-/**
- * @file index.js
- * @author ienix(enix@foxmail.com)
- *
- * @since 2017/11/9
- */
-/* global module, require*/
-
-const rem = require('main.js');
-const objectAssign =  require('object-assign');
-const LOADER_UTILS = require('loader-utils');
-
-const assign = Object.assign
-  ? Object.assign
-  : objectAssign;
-
-const REM = function (content, file, conf) {
-  var defaultConf = {
-    rem: 18,
-    min: 3,
-    dpr: 2,
-    fontSize2Rem: true,
-    exclude: []
-  };
-  var cfg = assign({}, defaultConf, conf);
-
-  return rem(content, file, cfg);
-};
-
-
-module.exports = function loader(content) {
-    const CONFIG = LOADER_UTILS.getOptions(this);
-
-    return REM(content, null, CONFIG);
-};
+module.exports = function (source, map) {
+  if (this.query.router && this.query.router.find(item => {return this.resourcePath.includes(item)})) {
+    source = source.replace(/rpx/gi, 'rem')
+  }
+  this.callback(null, source, map)
+}
